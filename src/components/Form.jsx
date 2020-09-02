21ee6d67ea45829265f6435864f003008f225bc5
juliocabrera820 <juliocabrera820@gmail.com>
2020-09-02 02:17:12 -0500

    feat: add yup validation and a new field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  firstName: yup.string().trim().required("first name is required"),
+  lastName: yup.string().trim().required("last name is required"),
+  age: yup
+    .number("age must be a number")
+    .positive("age must be positive")
+    .integer("age must a integer")
+    .required("age is required")
+    .typeError("you must specify a number"),
+});
 
 const Form = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
   const onSubmit = (data, e) => {
     console.log(data);
     e.target.reset();
   };
-
-  const WHITESPACES = "Whitespaces are not allowed";
 
   return (
     <div>
@@ -16,10 +29,7 @@ const Form = () => {
         <div className="form-group">
           <label>First Name</label>
           <input
-            ref={register({
-              required: "You must type a first name",
-              validate: (firstName) => firstName.trim() !== "" || WHITESPACES,
-            })}
+            ref={register}
             type="text"
             placeholder="Type first name"
             name="firstName"
@@ -35,10 +45,7 @@ const Form = () => {
         <div className="form-group">
           <label>Last Name</label>
           <input
-            ref={register({
-              required: "You must type a last name",
-              validate: (lastName) => lastName.trim() !== "" || WHITESPACES,
-            })}
+            ref={register}
             type="text"
             placeholder="Type last name"
             name="lastName"
@@ -49,6 +56,20 @@ const Form = () => {
           <div className="valid-feedback">Looks good!</div>
           <div className="invalid-feedback">
             {errors.lastName && errors.lastName.message}
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Age</label>
+          <input
+            ref={register}
+            type="text"
+            placeholder="Type age"
+            name="age"
+            className={`form-control ${errors.age ? "is-invalid" : "is-valid"}`}
+          />
+          <div className="valid-feedback">Looks good!</div>
+          <div className="invalid-feedback">
+            {errors.age && errors.age.message}
           </div>
         </div>
         <button className="btn btn-primary btn-block">Send</button>
